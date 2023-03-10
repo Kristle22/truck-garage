fca061d69f438aa309b35ae43602fa037275c1f0
Kristina <crislayn@yahoo.com>
2023-03-10 15:29:22 +0200

    Faker & fake car data provider

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +18,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker::create('lt_LT');
+        $faker->addProvider(new \Faker\Provider\Fakecar($faker));
+
+        DB::table('users')->insert([
+            'name' => 'Kristina',
+            'email' => 'crislayn@yahoo.com',
+            'password' => Hash::make('kriste22')
+        ]);
+
+        $mechCount = 20;
+        foreach(range(1, $mechCount) as $_) {
+            DB::table('mechanics')->insert([
+                'name' => $faker->firstName(),
+                'surname' => $faker->lastName()
+            ]);
+        }
+
+        $minYear = Carbon::now()->subYears(40)->format('Y');
+        foreach(range(1, 200) as $_) {
+            DB::table('trucks')->insert([
+                'maker' => $faker->company,
+                'plate' => $faker->vehicleRegistration,
+                'make_year' => $faker->year($max = 'now', $min = $minYear),
+                'mechanic_notices' => $faker->realText(rand(50, 100)),
+                'mechanic_id' => rand(1, $mechCount)
+            ]);
+        }
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
